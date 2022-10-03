67c48c171dd1214656e081521bdb72bc53602a8b
Asma HENDAOUI <asma.hendaoui.ext@lvmh.com>
2022-10-03 20:53:48 +0200

    First push

---
+++ Lucca.Converter/Models/ConverterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucca.Converter.Models
+{
+    public class ConverterModel
+    {
+        public string BaseCurrency { get; set; }
+        public string TargetCurrency { get; set; }
+
+        private double _exchangeRate;
+        public double  ExchangeRate   
+        {
+            get { return  Math.Round(_exchangeRate, 4); }  
+            set { _exchangeRate = value; }  
+        }
+    }
+}
---
+++ Lucca.Converter/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Lucca.Converter;
+using Lucca.Converter.Models;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World! this is back test \n");
+
+        Console.WriteLine("You need to insert Currency you want to convert FROM, Currency you want to convert TO and the amount in this format:\n FROM;Amount;TO \n example : EUR;550;JPY");
+        Console.WriteLine("\n");
+        var userChoice = Console.ReadLine();
+        Console.WriteLine("\n");
+        Console.WriteLine("nombre de ligne de currency");
+        int nbr = Convert.ToInt32(Console.ReadLine());
+        List<String> list = new List<String>();
+        for (int i = 0; i < nbr; i++)
+        {
+            list.Add(Console.ReadLine());
+        }
+
+        string exchangeRate = CurrencyConverter.GetTheShortestrateExchangeRate(userChoice, list);
+        Console.WriteLine(exchangeRate);
+
+        Console.ReadLine();
+    }
+}
